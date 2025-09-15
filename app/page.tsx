8370eb39c0c8b8d25ee3ed441a5cd4227c51509a
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-09-15 22:33:00 +0900

    LocalStorageに保存

--- app/page.tsx
+++ app/page.tsx
@@ -5,63 +5,67 @@ import ToDoList from "@/components/TodoList";
 import { ToDo } from "@/types/Todo";
 
 export default function Home() {
-  const [todos,setTodos] =useState<ToDo[]>([]);
-  const [text,setText] = useState("");
+  const [todos, setTodos] = useState<ToDo[]>([]);
+  const [text, setText] = useState("");
 
   useEffect(() => {
-    localStorage.setItem("todos",JSON.stringify(todos));
-  }, [todos]);
-  
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos));
+    }
+  }, []);
 
-  const addTodo = () =>{
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = () => {
     if (!text.trim()) return;
 
-    const newTodo: ToDo ={
+    const newTodo: ToDo = {
       id: Date.now(),
       text,
-      done:false,
+      done: false,
     };
 
-    setTodos([...todos,newTodo]);
+    setTodos([...todos, newTodo]);
     setText("");
   };
 
-  const toggleTodo = (id: number)=>{
+  const toggleTodo = (id: number) => {
     setTodos(
       todos.map((todo) =>
         todo.id === id ? { ...todo, done: !todo.done } : todo
       )
-    )
-  }
+    );
+  };
 
   const daleteTodo = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
-  
-return (
-  <div className="max-w-md mx-auto mt-10 p-4 border rounded shadow">
-    <h1 className="text-2xl font-bold mb-4"> ToDoリスト</h1>
 
-    <div className="flex gap-2 mb-4">
-      <input 
-      value={text} 
-      onChange={(e) => setText(e.target.value)}
-      className="flex-1 border p-2 rounded"
-      placeholder="Todoを入力"
-      />
-    
+  return (
+    <div className="max-w-md mx-auto mt-10 p-4 border rounded shadow">
+      <h1 className="text-2xl font-bold mb-4">ToDoリスト</h1>
 
-    <button
+      <div className="flex gap-2 mb-4">
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="flex-1 border p-2 rounded"
+          placeholder="Todoを入力"
+        />
+
+        <button
           onClick={addTodo}
           className="bg-blue-500 text-white px-4 py-2 rounded"
         >
-        追加
-    </button>
+          追加
+        </button>
+      </div>
+
+      <ToDoList todos={todos} onToggle={toggleTodo} onDelete={daleteTodo} />
     </div>
+  );
+}
 
-    <ToDoList todos={todos} onToggle={toggleTodo} onDelete={daleteTodo} />
-
-  </div>
-)
-
-};
